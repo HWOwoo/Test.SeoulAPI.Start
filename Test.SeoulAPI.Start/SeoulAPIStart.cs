706d45e0144d187fae829523b98e59441f54d15e
HWOwoo <101438486+HWOwoo@users.noreply.github.com>
2023-04-24 17:59:47 +0900

    123

--- Test.SeoulAPI.Start/SeoulAPIStart.cs
+++ Test.SeoulAPI.Start/SeoulAPIStart.cs
@@ -4,17 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Test.SeoulAPI;
+
+
 namespace Test.SeoulAPI.Start
 {
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length > 1)
+
+            if (args.Length == 0)
             {
                 Console.WriteLine("측정일자 yyyymmdd 입력");
-                
+                return;
             }
+
+            string date = args[0];
+            var apiStart = new SeoulAPIJson();
+            apiStart.ReadJsonData(date);
+
         }
     }
 }
---
+++ Test.SeoulAPI/SeoulAPIJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.SeoulAPI
+{
+    public class SeoulAPIJson
+    {
+        string inputKey = "5378624a4973757239367644476152";
+        string inputUrl = "http://openapi.seoul.go.kr:8088/{0}/json/DailyAverageRoadside/1/100/{1}";
+
+        public void ReadJsonData(string data)
+        {
+            string url = string.Format(inputUrl, inputKey, data);
+        }
+    }
+}
